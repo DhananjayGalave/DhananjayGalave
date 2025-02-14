635a55862871379097c18a0562da8f957a00c52e
DhananjayGalave <galave723@gmail.com>
2025-02-14 12:14:54 +0530

    Your descriptive commit message

---
+++ module.js
@@ -0,0 +1,25 @@
+
+export const hederSec = `<nav class="navbar navbar-expand-sm bg-black navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img class="rounded-circle" src="image/logo/DASG-Logo-White.png" alt="logo" height="80">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-md-flex justify-content-end" id="collapsibleNavbar">
+          <ul class="navbar-nav text-end">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contactus.html">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>`;
+
---
+++ script.js
@@ -0,0 +1,71 @@
+const hederSec = `<nav class="navbar navbar-expand-sm bg-black navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img class="rounded-circle" src="image/logo/DASG-Logo-White.png" alt="logo" height="80">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-md-flex justify-content-end" id="collapsibleNavbar">
+          <ul class="navbar-nav text-end">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contactus.html">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>`;
+
+    const footerSec =`<div class="container-fluid">
+      <div class="row mx-auto d-flex">
+        <div class="col-md-6 order-2 order-md-1 d-flex justify-content-center align-items-center pt-2">
+          <p class=""> &#xA9; 2025 Dhananjay Sudhir Galave | All Rights Reserved </p>
+        </div>
+        <div class="col-md-6 text text-center order-1 order-md-2">
+          <h6 class="">Social</h6>
+          <p>
+            <a href="https://github.com/DhananjayGalave" class="px-2 py-1 btn btn-outline-light btn-sm" target="_blank">
+              <i class="bi bi-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/dhananjay-galave/" class="px-2 py-1 btn btn-outline-light btn-sm"
+              target="_blank">
+              <i class="bi bi-linkedin"></i>
+            </a>
+            <a href="https://www.instagram.com/dhananjay_nap/" class="px-2 py-1 btn btn-outline-light btn-sm"
+              target="_blank">
+              <i class="bi bi-instagram fa-lg"></i>
+            </a>
+            <a href="https://www.facebook.com/dhananjay.nap/" class="px-2 py-1 btn btn-outline-light btn-sm"
+              target="_blank">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="mailto:galave723@gmail.com?subject=Inquiry from Your Website&body=Hello,%0D%0A%0D%0AI visited your website and would like to learn more about your services. Could you please provide more details?%0D%0A%0D%0AThank you!%0D%0A[Your Name]" class="px-2 py-1 btn btn-outline-light btn-sm"
+              target="_blank">
+              <!-- <i class="bi bi-envelope-at"></i> -->
+              <!-- <i class="fa-solid fa-at"></i> -->
+              <i class="fa-regular fa-envelope"></i>
+            </a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=galave723@gmail.com&su=Inquiry from Your Website&body=Hello,%0D%0A%0D%0AI visited your website and would like to know more about your services.%0D%0A%0D%0AThank you!" target="_blank" class="px-2 py-1 btn btn-outline-light btn-sm">
+              <!-- <i class="bi bi-envelope-at"></i> -->
+              <i class="fa-solid fa-at"></i>
+            </a>
+            <a href="https://api.whatsapp.com/send/?phone=918928282337&text=Hi,%20I%27d%20like%20to%20discuss%20a%20website%20project." class="btn btn-outline-light btn-sm" target="_blank" class="p-1">
+              <i class="fa-brands fa-whatsapp fa-lg"></i>
+            </a>
+            <a href="#" class="btn btn-outline-light btn-sm" class="p-1">
+              <i class="bi bi-arrow-up"></i>
+            </a>
+          </p>
+        </div>
+        <!-- <div class="col-md-3"></div> -->
+      </div>
+    </div>`;
+
+document.getElementById("headerCon").innerHTML = hederSec;
+document.getElementById("footerCon").innerHTML = footerSec;
